37a465870d75d478e4627d3ca185bb98672a2a66
Ruwindows <Ruwindows@Ruwindows-PC>
2016-04-28 19:25:31 -0300

    Implementando a IGC á IServer do pacote jshare.comun

--- JShare155848/src/br/univel/adriel/iu/InterfaceGraficaCliente.java
+++ JShare155848/src/br/univel/adriel/iu/InterfaceGraficaCliente.java
@@ -2,7 +2,9 @@ package br.univel.adriel.iu;
 
 import javax.swing.JFrame;
 
-public class InterfaceGraficaCliente extends JFrame{
+import br.dagostini.jshare.comun.IServer;
+
+public class InterfaceGraficaCliente extends JFrame implements IServer{
 	
 	
 	
