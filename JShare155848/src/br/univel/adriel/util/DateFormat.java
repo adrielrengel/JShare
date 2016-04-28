c7754b4fe273f1558d93c542c636067b48245e0a
Ruwindows <Ruwindows@Ruwindows-PC>
2016-04-28 19:21:19 -0300

    Implementação da classe DateFormat

--- JShare155848/src/br/univel/adriel/util/DateFormat.java
+++ JShare155848/src/br/univel/adriel/util/DateFormat.java
@@ -1,8 +1,17 @@
 package br.univel.adriel.util;
 
+import java.text.SimpleDateFormat;
+
 public class DateFormat {
 	
 	
+private SimpleDateFormat sdf;
 	
+	public SimpleDateFormat formatoData(){
+		
+		sdf = new SimpleDateFormat("dd/MM/yyyy H:mm:ss:SSS");
+		
+		return sdf;
+	}
 
 }
