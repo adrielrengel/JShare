268023a4cae7f4345f67c321154bfe4ef795cd78
adrielrengel <adrielrengal@gmail.com>
2016-05-02 19:57:37 -0300

    Implementado

---
+++ JShare155848/src/br/univel/adriel/model/TableModel.java
@@ -0,0 +1,75 @@
+package br.univel.adriel.model;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import javax.swing.table.AbstractTableModel;
+
+import br.dagostini.jshare.comum.pojos.Arquivo;
+import br.dagostini.jshare.comun.Cliente;
+
+
+public class TableModel extends AbstractTableModel implements TableModel {
+
+		private Object[][] matriz;
+		private int linhas;
+
+		public TableModel(Map<Cliente, List<Arquivo>> mapa) {
+
+			linhas = 0;
+			for (Entry<Cliente, List<Arquivo>> e : mapa.entrySet()) {
+				linhas+=e.getValue().size();
+			}
+
+			matriz = new Object[linhas][5];
+
+			int linha = 0;
+
+			for (Entry<Cliente, List<Arquivo>> e : mapa.entrySet()) {
+				for (Arquivo arq : e.getValue()) {
+					matriz[linha][0] = e.getKey().getNome();
+					matriz[linha][1] = e.getKey().getIp();
+					matriz[linha][2] = e.getKey().getPorta();
+					matriz[linha][3] = arq.getNome();
+					matriz[linha][4] = arq.getTamanho();
+					linha++;
+				}
+			}
+		}
+		
+		@Override
+		public String getColumnName(int columnIndex) {
+
+			switch (columnIndex) {
+			case 0:
+				return "Usuário";
+			case 1:
+				return "IP do Usuário";
+			case 2:
+				return "Porta do Usuário";
+			case 3:
+				return "Nome do Arquivo";
+			case 4:
+				return "Tamanho do Arquivo";
+			}
+			return null;
+
+		}
+
+		@Override
+		public int getColumnCount() {
+			return 5;
+		}
+
+		@Override
+		public int getRowCount() {
+			return linhas;
+		}
+
+		@Override
+		public Object getValueAt(int rowIndex, int columnIndex) {
+			return matriz[rowIndex][columnIndex];
+		}
+		
+}
