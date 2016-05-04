510d0bc28889d451dfab0f668ead70cc86cdc590
Ruwindows <Ruwindows@Ruwindows-PC>
2016-05-04 19:35:59 -0300

    era pra ser 3/10, mas acho que acabou entao 10/10

--- JShare155848/src/br/univel/adriel/model/LerGravarArquivo.java
+++ JShare155848/src/br/univel/adriel/model/LerGravarArquivo.java
@@ -8,23 +8,24 @@ import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 
 public class LerGravarArquivo {
-	
-	public LerGravarArquivo(File arquivo){
-		
-		byte[] dados = ler(arquivo);
-		
-		gravar(new File("Cópia de " + arquivo.getName()), dados);
-		
-		public void gravar(File arquivoNovo, byte[] dados){
-			try{
-				Files.write(Paths.get(arquivoNovo.getPath()), dados, StandardOpenOption.CREATE);
-			} catch(IOException e){
-				throw new RuntimeException(e);
-			}
+
+	public byte[] ler(File arquivoNovo){
+		Path path = Paths.get(arquivoNovo.getPath());
+		try{
+			byte[] dados = Files.readAllBytes(path);
+			return dados;
+		}catch(IOException e){
+			throw new RuntimeException(e);
 		}
+	}
 	
+	public void gravar(File arquivoNovo, byte[] dados){
+		try{
+			Files.write(Paths.get(arquivoNovo.getPath()), dados, StandardOpenOption.CREATE);
+		} catch(IOException e){
+			throw new RuntimeException(e);
+		}
+	}
 }
-	
-	
-	
-	
+
+
