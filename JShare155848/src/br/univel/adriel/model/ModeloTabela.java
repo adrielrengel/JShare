6e66393cebf6b63168e0f199abab9be62740c38d
Ruwindows <Ruwindows@Ruwindows-PC>
2016-05-05 19:59:52 -0300

    removido espeços que estavam me atrapalhando

--- JShare155848/src/br/univel/adriel/model/ModeloTabela.java
+++ JShare155848/src/br/univel/adriel/model/ModeloTabela.java
@@ -13,64 +13,65 @@ import br.dagostini.jshare.comun.Cliente;
 
 public class ModeloTabela extends AbstractTableModel implements TableModel {
 
-		private Object[][] matriz;
-		private int linhas;
+	private Object[][] matriz;
+	private int linhas;
 
-		public ModeloTabela(Map<Cliente, List<Arquivo>> mapa) {
+	public ModeloTabela(Map<Cliente, List<Arquivo>> mapa) {
 
-			linhas = 0;
-			for (Entry<Cliente, List<Arquivo>> e : mapa.entrySet()) {
-				linhas+=e.getValue().size();
+		linhas = 0;
+		for (Entry<Cliente, List<Arquivo>> e : mapa.entrySet()) {
+			linhas+=e.getValue().size();
+		}
+
+		matriz = new Object[linhas][5];
+
+		int linha = 0;
+
+		for (Entry<Cliente, List<Arquivo>> e : mapa.entrySet()) {
+			for (Arquivo arq : e.getValue()) {
+				matriz[linha][0] = e.getKey().getNome();
+				matriz[linha][1] = e.getKey().getIp();
+				matriz[linha][2] = e.getKey().getPorta();
+				matriz[linha][3] = arq.getNome();
+				matriz[linha][4] = arq.getTamanho();
+				linha++;
 			}
-
-			matriz = new Object[linhas][5];
-
-			int linha = 0;
-
-			for (Entry<Cliente, List<Arquivo>> e : mapa.entrySet()) {
-				for (Arquivo arq : e.getValue()) {
-					matriz[linha][0] = e.getKey().getNome();
-					matriz[linha][1] = e.getKey().getIp();
-					matriz[linha][2] = e.getKey().getPorta();
-					matriz[linha][3] = arq.getNome();
-					matriz[linha][4] = arq.getTamanho();
-					linha++;
-				}
-			}
 		}
-		
-		@Override
-		public String getColumnName(int columnIndex) {
+	}
+	
+	@Override
+	public String getColumnName(int columnIndex) {
 
-			switch (columnIndex) {
-			case 0:
-				return "USER";
-			case 1:
-				return "IP";
-			case 2:
-				return "Porta";
-			case 3:
-				return "Arquivo";
-			case 4:
-				return "Tamanho";
-			}
-			return null;
-
+		switch (columnIndex) {
+		case 0:
+			return "Usuário";
+		case 1:
+			return "IP do Usuário";
+		case 2:
+			return "Porta do Usuário";
+		case 3:
+			return "Nome do Arquivo";
+		case 4:
+			return "Tamanho do Arquivo";
 		}
+		return null;
 
-		@Override
-		public int getColumnCount() {
-			return 5;
-		}
+	}
 
-		@Override
-		public int getRowCount() {
-			return linhas;
-		}
+	@Override
+	public int getColumnCount() {
+		return 5;
+	}
 
-		@Override
-		public Object getValueAt(int rowIndex, int columnIndex) {
-			return matriz[rowIndex][columnIndex];
-		}
-		
+	@Override
+	public int getRowCount() {
+		return linhas;
+	}
+
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		return matriz[rowIndex][columnIndex];
+	}
+
+
 }
