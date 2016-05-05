3f76488b6ddc3debf3d2cf97dfde9f305178cc36
Ruwindows <Ruwindows@Ruwindows-PC>
2016-05-05 19:58:04 -0300

    uhull

--- JShare155848/src/br/univel/adriel/iu/InterfaceGraficaServidor.java
+++ JShare155848/src/br/univel/adriel/iu/InterfaceGraficaServidor.java
@@ -355,36 +355,7 @@ public class InterfaceGraficaServidor extends JFrame implements IServer{
 	 * ===================================================================================================
 	 */
 
-	// Método que faz a busca por IPs disponíveis.
-	private List<String> buscaIp() {
 
-		List<String> addrList = new ArrayList<String>();
-		try {
-			Enumeration<NetworkInterface> ifaces = NetworkInterface.getNetworkInterfaces();
-
-			while (ifaces.hasMoreElements()) {
-				NetworkInterface ifc = ifaces.nextElement();
-				if (ifc.isUp()) {
-					Enumeration<InetAddress> addresses = ifc.getInetAddresses();
-					while (addresses.hasMoreElements()) {
-
-						InetAddress addr = addresses.nextElement();
-
-						String ip = addr.getHostAddress();
-
-						if (ip.matches("[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}")) {
-							addrList.add(ip);
-						}
-
-					}
-				}
-			}
-		} catch (SocketException e) {
-			e.printStackTrace();
-		}
-
-		return addrList;
-	}
 
 	// Método utilizado para bloquear o comboBox e o textField.
 	private void bloquearCampos(Boolean status) {
